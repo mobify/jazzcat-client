812db1be4a61399d1ea44edb6f12fe3856a23644
Shawn Jansepar <shawn@mobify.com>
2014-03-24 16:39:57 -0700

    Removing comment that is no longer relevant

--- jazzcat.js
+++ jazzcat.js
@@ -549,7 +549,7 @@
                (options.cacheBreaker ? '/cb' + options.cacheBreaker : '') +
                '/' + options.responseType +
                (options.responseType === 'jsonp' ? '/' + options.loadCallback : '') +
-               '/' + encodeURIComponent(JSON.stringify(urls.slice().sort())); // TODO only sort for jsonp
+               '/' + encodeURIComponent(JSON.stringify(urls.slice().sort()));
     };
 
     var scriptSplitRe = /(<\/scr)(ipt\s*>)/ig;
